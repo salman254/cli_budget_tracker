53596a90e3ef2c11deda4e65f78c08ec92a0d5db
Salman <salmaanmohamed700@gmail.com>
2025-05-27 20:43:58 +0300

    Deleted unnecesary file

--- budget.py
+++ budget.py
@@ -1,16 +1,1 @@
 
-from db.models import Budget
-
-def view_budgets(session, user):
-    budgets = session.query(Budget).filter_by(user_id=user.id).all()
-
-    if budgets:
-        for b in budgets:
-            print(f"ID: {b.id}")
-            print(f"Category: {b.category.name}")
-            print(f"Amount: {b.amount}")
-            print(f"Period: {b.period}")
-            print("-" * 30)
-    else:
-        print("No budgets found.")
-    input("Press Enter to continue...")
