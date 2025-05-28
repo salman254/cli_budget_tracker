b87a378552c766184b95d7c458453c5aa27a4efe
Salman <salmaanmohamed700@gmail.com>
2025-05-28 18:13:06 +0300

    Updated Cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,10 @@
 from db.models import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from lib.category import view_categories
-from lib.transaction import add_transaction, view_transactions
-from lib.budget import view_budgets
-from lib.budget import report_budget_status
 
+from lib.category import category_menu
+from lib.transaction import transaction_menu
+from lib.budget import budget_menu
 
 engine = create_engine("sqlite:///db/budget.db")
 Session = sessionmaker(bind=engine)
@@ -16,29 +15,22 @@ user = session.query(User).filter_by(username="salman").first()
 def main_menu():
     while True:
         print("\nBudget Tracker CLI")
-        print("1. View Categories")
-        print("2. Add Transaction")
-        print("3. View Budgets")
-        print("4. View Transactions")
-        print("5. View Budget Reports")
-        print("6. Exit")
-
+        print("1. Manage Categories")
+        print("2. Manage Transactions")
+        print("3. Manage Budgets")
+        print("4. Exit")
 
         choice = input("Select an option: ")
 
         if choice == "1":
-            view_categories(session, user)
+            category_menu(session, user)
         elif choice == "2":
-            add_transaction(session, user)
+            transaction_menu(session, user)
         elif choice == "3":
-            view_budgets(session, user)
+            budget_menu(session, user)
         elif choice == "4":
-            view_transactions(session, user)
-        elif choice == "5":
-                report_budget_status(session, user)
-        elif choice == "6":
             print("Goodbye!")
             break
-
         else:
             print("Invalid choice. Please try again.")
+            input("Press Enter to continue...")
